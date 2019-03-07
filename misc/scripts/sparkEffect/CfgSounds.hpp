dcddb7e8af732d5208b24117085371803269184f
Katalam <goerss99@gmail.com>
2019-03-07 02:03:27 +0100

    Added spark effect function

---
+++ misc/scripts/sparkEffect/CfgSounds.hpp
@@ -0,0 +1,37 @@
+class CfgSounds {
+	class spark1 {
+		name = "spark1";
+		sound[] = {"\sound\spark1.ogg", db+30, 1};
+		titles[] = {};
+	};
+	class spark2 {
+		name = "spark2";
+		sound[] = {"\sound\spark2.ogg", db+30, 1};
+		titles[] = {};
+	};
+	class spark3 {
+		name = "spark3";
+		sound[] = {"\sound\spark3.ogg", db+30, 1};
+		titles[] = {};
+	};
+	class spark4 {
+		name = "spark4";
+		sound[] = {"\sound\spark4.ogg", db+30, 1};
+		titles[] = {};
+	};
+	class spark5 {
+		name = "spark5";
+		sound[] = {"\sound\spark5.ogg", db+30, 1};
+		titles[] = {};
+	};
+	class spark11 {
+		name = "spark11";
+		sound[] = {"\sound\spark11.ogg", db+30, 1};
+		titles[] = {};
+	};
+	class spark22 {
+		name = "spark22";
+		sound[] = {"\sound\spark22.ogg", db+30, 1};
+		titles[] = {};
+	};
+};
